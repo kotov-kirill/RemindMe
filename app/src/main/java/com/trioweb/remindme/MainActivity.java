e3afc2461a098642155f17510b117938329c64f8
kotov-kirill <kirsoft95@mail.ru>
2016-01-22 00:05:26 +0300

    Add Buttons on NavigationView

--- app/src/main/java/com/trioweb/remindme/MainActivity.java
+++ app/src/main/java/com/trioweb/remindme/MainActivity.java
@@ -1,12 +1,14 @@
 package com.trioweb.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.setTheme(R.style.AppDefault);
@@ -17,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initComponents(){
         this.initToolbar();
+        this.initNavigationView();
     }
+
     private void initToolbar(){
         this.toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         this.toolbar.setTitle(R.string.app_name);
@@ -28,5 +32,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         this.toolbar.inflateMenu(R.menu.menu_main);
+    }
+
+    private void initNavigationView() {
+        this.drawerLayout = (DrawerLayout) this.findViewById(R.id.drawer_layout);
     }
 }
